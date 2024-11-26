14171ebf716f01899c9aeac0d7ef275bd2ced01c
bhdrdegirmenci@outlook.com <bhdrdegirmenci@outlook.com>
2024-11-26 15:50:29 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.DataAccsesLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CSharpEgitimKampi301.DataAccsesLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        void Insert(T entity);
+        CodeIdentifier Update(T entity);
+        void Delete(int id);
+        List<T> GetAll();
+        T GetById(int id);
+
+    }
+}
---
+++ CSharpEgitimKampi301.DataAccsesLayer/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CSharpEgitimKampi301.DataAccsesLayer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CSharpEgitimKampi301.DataAccsesLayer")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e2921e8e-417c-4802-96b9-045ed2c98dde")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
